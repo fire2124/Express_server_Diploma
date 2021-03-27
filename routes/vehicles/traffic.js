abb90a2d7bde5d8665eb99988facaab7948c9088
fire2124 <filip.reichl@gmail.com>
2021-03-27 11:28:04 +0100

    remap coordinates to lan, lot

--- routes/vehicles/traffic.js
+++ routes/vehicles/traffic.js
@@ -1,17 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-const firstJsonUrl = "http://localhost:9500/api/v1/currentTraffic/firstJSON/1"
-const traffic = require('../../Data/currentMocups/traffic.json')
+const firstJsonUrl = "http://localhost:9500/api/v1/currentTraffic/firstJSON/1";
+const traffic = require("../../Data/currentMocups/traffic.json");
 
 router.get("/", async (req, res) => {
   // let response = await axios.get(firstJsonUrl);
   // response = response.data;
-  let responseFinal = []
-  traffic.map(e=>{
-    if(e.geometry)
-    responseFinal.push(e)
-  })
+  let responseFinal = [];
+  traffic.map((e) => {
+    if (
+      e.geometry &&
+      (e.geometry.type === "LineString" || e.geometry.type === "Point")
+    ) {
+      let obj = {};
+      obj.type = e.type;
+      let geometry = {};
+      geometry.type = e.geometry.type;
+      let coordinates = [];
+      if (e.geometry.type === "LineString") {
+        e.geometry.coordinates.map((x) => {
+          let array = [];
+          console.log(x);
+          array.push(x[1], x[0]);
+          coordinates.push(array);
+        });
+      } else if (e.geometry.type === "Point") {
+        coordinates.push(e.geometry.coordinates[1], e.geometry.coordinates[0]);
+      }
+      geometry.coordinates = coordinates;
+      obj.geometry = geometry;
+      obj.properties = e.properties;
+      responseFinal.push(obj);
+    }
+  });
   res.send(responseFinal);
 });
 
